eca7475b559a71d3f39311aa77e360ee814b4c73
DONG IL <cappu159@gmail.com>
2023-01-29 12:54:52 +0900

    ✨ feat(*): add cors in server & Client logic completed

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,18 @@
 import express, { Express, Request, Response } from 'express'
 import mongoose from 'mongoose'
 import { getAllRouter, addBookRouter, getBookRouter } from './routes'
+import cors from 'cors'
 
 const app: Express = express()
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors())
 app.use('/books', getAllRouter)
-// app.use('/books/:id', getBookRouter)
 
 mongoose.set('strictQuery', true)
 
-//uZflKsjWb2HGvsY4
 mongoose
   .connect(
     'mongodb+srv://admin:uZflKsjWb2HGvsY4@cluster0.zhnsshv.mongodb.net/?retryWrites=true&w=majority'
--- server/dist/app.js
+++ server/dist/app.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use('/books', routes_1.getAllRouter);
-// app.use('/books/:id', getBookRouter)
 mongoose_1.default.set('strictQuery', true);
-//uZflKsjWb2HGvsY4
 mongoose_1.default
     .connect('mongodb+srv://admin:uZflKsjWb2HGvsY4@cluster0.zhnsshv.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Mongoose connected'))
